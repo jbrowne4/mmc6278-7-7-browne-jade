c53b4a11f4175b91d1628622aef986cafa28d6f8
Jade Browne <jade.browne@outlook.com>
2025-03-09 17:01:49 -0400

    initial

--- db.js
+++ db.js
@@ -1,12 +1,11 @@
-const mysql = require('mysql2')
+const mysql = require("mysql2");
 
 const config = process.env.JAWSDB_URL || {
   connectionLimit: 10,
   host: "localhost",
   user: "root",
-  database: "auth_db",
-}
+  database: process.env.npm_package_config_DB_NAME,
+};
+const db = mysql.createPool(config);
 
-const db = mysql.createPool(config)
-
-module.exports = db.promise()
+module.exports = db.promise();
--- index.js
+++ index.js
@@ -1,52 +1,10 @@
-const express = require('express')
-const bcrypt = require('bcrypt')
-const PORT = process.env.PORT || 3000
-const db = require('./db')
-const app = express()
+const app = require("./app");
+const PORT = process.env.PORT || 3000;
 
-app.use(express.urlencoded({extended: false}))
-
-app.use(express.static('public'))
-
-app.get('/user', async (req, res) => {
-  try {
-    const [users] = await db.query(`SELECT * FROM users`)
-    res.json(users)
-  } catch(err) {
-    res.status(500).send("error retrieving users:" + err.message || err.sqlMessage)
-  }
-})
-
-app.post('/user', async (req, res) => {
-  try {
-    const { username, password } = req.body
-    if (!(username && password))
-      return res.status(400).send('must include username and password')
-
-    // TODO: Hash password and insert user
-
-    res.redirect('/user')
-  } catch(err) {
-    if (err.code === 'ER_DUP_ENTRY')
-      return res.status(409).send('User already exists')
-    res.status(500).send('Error Creating User: ' + err.message || err.sqlMessage)
-  }
-})
-
-app.post('/login', async(req, res) => {
-  try {
-    const { username, password } = req.body
-    if (!(username && password))
-      return res.status(400).send('must include username and password')
-
-    // TODO: Retrieve User, compare hash
-
-    res.json({...user, isCorrectPassword})
-  } catch(err) {
-    res.status(500).send('Error Creating User: ' + err.message || err.sqlMessage)
-  }
-})
-
-app.listen(PORT, () => {
-  console.log('listening on http://localhost:' + PORT)
-})
+app.listen(PORT, function () {
+  console.log("\n");
+  console.log(
+    "\x1b[35m%s\x1b[0m",
+    "Server listening on http://localhost:" + PORT
+  );
+});
